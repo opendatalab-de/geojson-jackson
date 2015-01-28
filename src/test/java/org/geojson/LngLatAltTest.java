9c6b31b03dc1546d1bb31e4989ed50c95a0153dc
William Delanoue <william.delanoue@deveryware.net>
2015-01-28 14:05:04 +0100

    add equals/hashCode on LngLatAlt

---
+++ src/test/java/org/geojson/LngLatAltTest.java
@@ -0,0 +1,40 @@
+package org.geojson;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LngLatAltTest {
+
+
+	@Test
+	public void should_LngLatAlt_equals_without_alt() {
+		LngLatAlt first = new LngLatAlt(14.D, 13.D);
+		LngLatAlt second = new LngLatAlt(14.D, 13.D);
+
+		Assert.assertEquals(second, first);
+	}
+
+	@Test
+	public void should_LngLatAlt_equals_with_alt() {
+		LngLatAlt first = new LngLatAlt(14.D, 13.D, 15D);
+		LngLatAlt second = new LngLatAlt(14.D, 13.D, 15D);
+
+		Assert.assertEquals(second, first);
+	}
+
+	@Test
+	public void should_not_LngLatAlt_equals_with_alt() {
+		LngLatAlt first = new LngLatAlt(14.D, 13.D, 15D);
+		LngLatAlt second = new LngLatAlt(14.D, 13.D, 16D);
+
+		Assert.assertNotEquals(second, first);
+	}
+
+	@Test
+	public void should_not_LngLatAlt_equals_without_alt() {
+		LngLatAlt first = new LngLatAlt(14.D, 14.D, 15D);
+		LngLatAlt second = new LngLatAlt(14.D, 13.D, 16D);
+
+		Assert.assertNotEquals(second, first);
+	}
+}
