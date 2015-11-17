203903550f9a7c5c3388f27a2018f877ad7edba7
VIEGAS Ludovic <ludovic.viegas@thalesgroup.com>
2015-11-17 10:12:56 +0100

    add GeoJsonObjectVisitor.Adapter

--- src/main/java/org/geojson/GeoJsonObjectVisitor.java
+++ src/main/java/org/geojson/GeoJsonObjectVisitor.java
@@ -25,4 +25,59 @@ public interface GeoJsonObjectVisitor<T> {
 	T visit(MultiPoint geoJsonObject);
 
 	T visit(LineString geoJsonObject);
+
+	/**
+	 * An abstract adapter class for visiting GeoJson objects.
+	 * The methods in this class are empty.
+	 * This class exists as convenience for creating listener objects.
+	 *
+	 * @param <T> Return type of the visitor
+   */
+	class Adapter<T> implements GeoJsonObjectVisitor<T> {
+
+		@Override
+		public T visit(GeometryCollection geoJsonObject) {
+			return null;
+		}
+
+		@Override
+		public T visit(FeatureCollection geoJsonObject) {
+			return null;
+		}
+
+		@Override
+		public T visit(Point geoJsonObject) {
+			return null;
+		}
+
+		@Override
+		public T visit(Feature geoJsonObject) {
+			return null;
+		}
+
+		@Override
+		public T visit(MultiLineString geoJsonObject) {
+			return null;
+		}
+
+		@Override
+		public T visit(Polygon geoJsonObject) {
+			return null;
+		}
+
+		@Override
+		public T visit(MultiPolygon geoJsonObject) {
+			return null;
+		}
+
+		@Override
+		public T visit(MultiPoint geoJsonObject) {
+			return null;
+		}
+
+		@Override
+		public T visit(LineString geoJsonObject) {
+			return null;
+		}
+	}
 }
