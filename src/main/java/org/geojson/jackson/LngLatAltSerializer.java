e8409a9d38019a88cc74bddb6cce85f244728a7a
unknown <jones_s-r@ESDR-LTH4X5R72.scisys-r.local>
2017-01-31 14:35:22 +0000

    LngLatAltSerializer test added and unused code removed

--- src/main/java/org/geojson/jackson/LngLatAltSerializer.java
+++ src/main/java/org/geojson/jackson/LngLatAltSerializer.java
@@ -1,30 +1,33 @@
 package org.geojson.jackson;
 
+import java.io.IOException;
+
+import org.geojson.LngLatAlt;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import org.geojson.LngLatAlt;
 
-import java.io.IOException;
+public class LngLatAltSerializer extends JsonSerializer<LngLatAlt>
+{
 
-public class LngLatAltSerializer extends JsonSerializer<LngLatAlt> {
+    @Override
+    public void serialize(LngLatAlt value, JsonGenerator jgen, SerializerProvider provider)
+        throws IOException, JsonProcessingException
+    {
+        jgen.writeStartArray();
+        jgen.writeNumber(value.getLongitude());
+        jgen.writeNumber(value.getLatitude());
+        if (value.hasAltitude())
+        {
+            jgen.writeNumber(value.getAltitude());
 
-	public static final long POW10[] = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
-
-	@Override
-	public void serialize(LngLatAlt value, JsonGenerator jgen, SerializerProvider provider) throws IOException,
-			JsonProcessingException {
-		jgen.writeStartArray();
-		jgen.writeNumber(value.getLongitude());
-		jgen.writeNumber(value.getLatitude());
-		if (value.hasAltitude()) {
-			jgen.writeNumber(value.getAltitude());
-
-			for(double d : value.getAdditionalElements()) {
-				jgen.writeNumber(d);
-			}
-		}
-		jgen.writeEndArray();
-	}
+            for (double d : value.getAdditionalElements())
+            {
+                jgen.writeNumber(d);
+            }
+        }
+        jgen.writeEndArray();
+    }
 }
